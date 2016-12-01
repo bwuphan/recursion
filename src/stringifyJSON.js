a0a48b9921e5491d2778dbae4571875e568ccf35
Bobby <bwuphan@gmail.com>
2016-12-01 12:22:37 -0800

    Finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj === 'string'){
+  	return '"' + obj + '"';
+  }
+  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null){
+  	return '' + obj;
+  }
+  if(Array.isArray(obj)){
+  	if(obj[0] === undefined){
+  		return "" + "[]";
+  	} else {
+  		let arrayString = "[";
+  		for(let i = 0; i < obj.length; i++){
+  			arrayString += stringifyJSON(obj[i]);
+  			if(i === obj.length - 1){
+  				arrayString += "]";
+  			} else {
+  				arrayString += ",";
+  			}
+  		}
+  		return arrayString;
+  	}
+  }
+  if(obj instanceof Object){
+  	let objectString = "{";
+  	for(let key in obj){
+  		if(obj[key] !== undefined && !(key === "functions" && stringifyJSON(obj[key]) === "{}")){
+  			objectString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ",";
+  		}
+  	}
+  	if(objectString.length > 1){
+  		objectString = objectString.slice(0, objectString.length - 1);
+  	}
+  	objectString += "}";
+  	return objectString;
+  }
 };
